9cd6e622ddaaca6277a77834c560ef1bc52072f8
cuong11996 <cuong.hs96@gmail.com>
2022-01-11 21:08:59 +0700

    Cuong create

---
+++ ArchitectureAndDesign/src/main/java/com/rmit/ArchitectureAndDesign/ArchitectureAndDesignApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.ArchitectureAndDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureAndDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureAndDesignApplication.class, args);
+	}
+
+}
